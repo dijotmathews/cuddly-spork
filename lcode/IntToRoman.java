d4508feea0e3594ffad7e8bec985e18c95478368
Dijo Mathews <dijo.mathews@gmail.com>
2025-01-01 19:21:02 +0530

    int to roman

--- lcode/IntToRoman.java
+++ lcode/IntToRoman.java
@@ -1,22 +1,32 @@
-import java.util.HashMap;
-
+//https://www.youtube.com/watch?v=ohBNdSJyLh8
 public class IntToRoman {
     public static String intToRoman(int num) {
         String[] r = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
         int[] n =    {1000, 900, 500, 400,  100,  90,  50,   40, 10, 9, 5, 4, 1 };
 
-        String result = "";
+        StringBuilder result = new StringBuilder();
 
         for(int i = 0; i < n.length; i++) {
             int part = num / n[i];
             if(part != 0) {
-                break;
+                result.append(catStr(r[i], part));
+                num = num % n[i];
             }
-            System.out.println(part);
 
-            num = num % n[i];
+
         }
-        return  result;
+        return  result.toString();
+    }
+
+    public static String catStr(String str, int n) {
+        if (str == null || n <= 0) {
+            return "";
+        }
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < n; i++) {
+            result.append(str);
+        }
+        return result.toString();
     }
 
     public static void main(String[] arg) {
