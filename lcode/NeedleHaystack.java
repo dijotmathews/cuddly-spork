9e40d27f1bafc2a3971f2a0712fc031c6edeb0ed
Dijo Mathews <dijo.mathews@hatio.in>
2025-01-03 11:47:09 +0530

    all easy array/string

---
+++ lcode/NeedleHaystack.java
@@ -0,0 +1,11 @@
+public class NeedleHaystack {
+
+    public static int strStr(String haystack, String needle) {
+        return haystack.indexOf(needle);
+    }
+    public static void main(String[] args) {
+//        String haystack = "sadbutsad", needle = "sad";
+        String haystack = "leetcode", needle = "leeto";
+        System.out.println(strStr(haystack, needle));
+    }
+}
